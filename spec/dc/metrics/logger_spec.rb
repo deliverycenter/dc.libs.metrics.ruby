83a7a37b9d27bd26aceb1e09a4acc1ae12355f33
Lucas Braz Cunha <lucas@deliverycenter.com>
2020-11-18 15:21:02 -0300

    Added comment to spec class

--- spec/dc/metrics/logger_spec.rb
+++ spec/dc/metrics/logger_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# TODO: Improve tests to cover the class behaviour
 RSpec.describe Dc::Metrics::Logger do
   describe 'Class instance interface' do
     it 'can log a message' do
